f0373486580e62a6ed08f0c8a30f8427aeb3576f
Theo Savage <tcrsavage@gmail.com>
2016-04-07 17:58:36 +0100

    Initial commit

---
+++ inc/classes/class-mailjet-wp-mail.php
@@ -0,0 +1,245 @@
+<?php
+
+/**
+ * API Integration for Mailjet transactional emails.
+ *
+ * Class Mailjet_WP_Mail
+ */
+class Mailjet_WP_Mail {
+
+	/**
+	 * Mailjet base API URL
+	 *
+	 * @var string
+	 */
+	static $base_url = 'https://api.mailjet.com/v3/';
+
+	/**
+	 * Send an email via mailjet (accepts wp_mail style arguments)
+	 *
+	 * @param array $to                   Send to email can be comma separated list or array of emails
+	 * @param string $subject             Subject string
+	 * @param string $message             Message string (expects html markup)
+	 * @param string $headers             Headers string (Reply-To, CC etc)
+	 * @param array $attachments          Attachments array of file paths
+	 * @param array $request_body         Override array to define explicit API call body params (overrides internal handling and filtering)
+	 */
+	static function send( $to, $subject, $message, $headers = '', $attachments = array(), $request_body = array() ) {
+
+		$body = array(
+			'Subject'     => $subject,
+			'Html-part'   => $message,
+			'Recipients'  => array_map( function( $email ) {
+				return (object) array( 'Email' => $email );
+			}, (array) $to )
+		);
+
+		$body = static::parse_send_args( $to, $subject, $message, $headers, $attachments, $body );
+
+		$body = wp_parse_args( $request_body, $body );
+
+		static::request( 'POST', 'send', $body );
+	}
+
+	/**
+	 * Make a request to the Mailjet v3 API
+	 *
+	 * @param $method
+	 * @param $path
+	 * @param $body
+	 * @param array $headers
+	 * @return bool
+	 */
+	static function request( $method, $path, $body, $headers = array() ) {
+
+		$http = new \WP_Http();
+
+		$response = $http->request( static::$base_url . $path, array(
+			'method'       => $method,
+			'timeout'      => 15,
+			'httpversion'  => '1.1',
+			'headers'      => wp_parse_args( $headers, array(
+				'Content-Type'   => 'application/json',
+				'Authorization'  => sprintf( 'Basic %s', base64_encode( sprintf( '%s:%s', MAILJET_API_KEY, MAILJET_SECRET_KEY ) ) )
+			) ),
+			'body' => json_encode( $body )
+		) );
+
+		if ( is_wp_error( $response ) || empty( $response['response']['code'] ) || $response['response']['code'] > 200 ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Parse wp_mail compatible args for use with the Mailjet API
+	 *
+	 * @param $to
+	 * @param $subject
+	 * @param $message
+	 * @param $headers
+	 * @param $attachments
+	 * @param $body
+	 * @return mixed|void
+	 */
+	protected static function parse_send_args( $to, $subject, $message, $headers, $attachments, $body ) {
+
+		$body = static::parse_to_email( $to, $body );
+		$body = static::parse_headers( $headers, $body );
+		$body = static::parse_attachments( $attachments, $body );
+
+		return apply_filters( 'mailjet_wp_mail_body', $body );
+	}
+
+	/**
+	 * Parse wp_mail compatible to email for use with the Mailjet API
+	 *
+	 * @param $to
+	 * @param $body
+	 * @return mixed|void
+	 */
+	protected static function parse_to_email( $to, $body ) {
+
+		if ( ! is_array( $to ) ) {
+			$to = explode( ',', $to );
+		}
+
+		return apply_filters( 'mailjet_wp_mail_to_email', $to );
+	}
+
+	/**
+	 * Parse wp_mail compatible headers for use with the Mailjet API
+	 *
+	 * @param $headers
+	 * @param $body
+	 * @return array|mixed|void
+	 */
+	protected static function parse_headers( $headers, $body ) {
+
+		if ( ! is_array( $body ) ) {
+			return $body;
+		}
+
+		// Prepare the passed headers.
+		if ( ! is_array( $headers ) ) {
+			$headers = explode( "\n", str_replace( "\r\n", "\n", $headers ) );
+		}
+
+		// Bail if we don't have any headers to work with.
+		if ( empty( $headers ) ) {
+			return $body;
+		}
+
+		foreach ( (array) $headers as $index => $header ) {
+
+			if ( false === strpos( $header, ':' ) ) {
+				continue;
+			}
+
+			// Explode them out
+			list( $name, $content ) = explode( ':', trim( $header ), 2 );
+
+			// Cleanup crew
+			$name    = trim( $name );
+			$content = trim( $content );
+
+			switch ( strtolower( $name ) ) {
+
+				// Mailjet handles these separately
+				case 'subject':
+				case 'to':
+					unset( $headers[ $index ] );
+					break;
+
+				case 'from':
+
+					$bracket_pos = strpos( $content, '<' );
+
+					if ( $bracket_pos !== false ) {
+
+						// Text before the bracketed email is the "From" name.
+						if ( $bracket_pos > 0 ) {
+							$from_name = trim( str_replace( '"', '', substr( $content, 0, $bracket_pos - 1 ) ) );
+						}
+
+						$from_email = trim( str_replace( '>', '', substr( $content, $bracket_pos + 1 ) ) );
+
+						// Avoid setting an empty $from_email.
+					} elseif ( '' !== trim( $content ) ) {
+
+						$from_email = trim( $content );
+					}
+
+					break;
+
+				default:
+					// Add it to our grand headers array
+					$body['Headers'][ trim( $name ) ] = trim( $content );
+					break;
+			}
+		}
+
+		// Need a default from email
+		if ( ! isset( $from_email ) ) {
+
+			// Get the site domain and get rid of www.
+			$sitename   = strtolower( $_SERVER['SERVER_NAME'] );
+			$sitename   = ( substr( $sitename, 0, 4 ) == 'www.' ) ? substr( $sitename, 4 ) : $sitename;
+			$from_email = 'wordpress@' . $sitename;
+		}
+
+		// Need a default from name
+		if ( ! isset( $from_name ) ) {
+			$from_name  = 'Wordpress';
+		}
+
+		$body['FromName']  = apply_filters( 'wp_mail_from_name', $from_name );
+		$body['FromEmail'] = apply_filters( 'wp_mail_from', $from_email );
+
+		return apply_filters( 'mailjet_wp_mail_headers', $body );
+	}
+
+	/**
+	 * Parse wp_mail compatible attachments for use with the Mailjet API
+	 *
+	 * @param $attachments
+	 * @param $body
+	 * @return mixed|void
+	 */
+	protected static function parse_attachments( $attachments, $body ) {
+
+		// Ensure we are working with an array of file paths
+		if ( ! is_array( $attachments ) ) {
+			$attachments = explode( "\n", str_replace( "\r\n", "\n", $attachments ) );
+		}
+
+		// Create attachment objects from file paths
+		$attachments = array_map( function( $attachment ) {
+
+			// File not available, skip
+			if ( ! is_readable( $attachment ) || ! mime_content_type( $attachment ) ) {
+				return false;
+			}
+
+			// Get file name
+			$name = end( ( explode( '/', str_replace( '\\', '/', $attachment ) ) ) );
+
+			//Create object with content type, name and base64 encoded contents
+			return array(
+				'Content-type' => mime_content_type( $attachment ),
+				'Filename'     => $name,
+				'content'      => base64_encode( file_get_contents( $attachment ) )
+			);
+
+		}, $attachments );
+
+		// Filter attachments which couldn't be parsed
+		$attachments = array_filter( $attachments );
+
+		// Add to send request body
+		$body['Attachments'] = $attachments;
+
+		return apply_filters( 'mailjet_wp_mail_attachments', $body );
+	}
+}
---
+++ mailjet-wp-mail.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name:  Mailjet wp_mail
+ * Plugin URI:   https://github.com/tcrsavage/mailjet-wp-mail
+ * Description:  Drop-in replacement for standard wp_mail function using mailjet API integration
+ * Version:      0.0.1
+ * Author:       tcrsavage, sanchothefat
+ * Author URI:   https://github.com/tcravage
+ * License:      GPL-2.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'MAILJET_API_KEY' ) || ! defined( 'MAILJET_SECRET_KEY' ) ) {
+	return;
+}
+
+require_once __DIR__ . '/inc/classes/class-mailjet-wp-mail.php';
+
+/**
+ * Override WordPress' default wp_mail function with one that sends email
+ * using Mailjet's API.
+ *
+ * Note that this function requires the MAILJET_API_KEY and MAILJET_SECRET_KEY constants to be defined
+ * in order for it to work. The easiest place to define this is in wp-config.
+ *
+ * @since  0.0.1
+ * @access public
+ * @param  string $to
+ * @param  string $subject
+ * @param  string $message
+ * @param  mixed $headers
+ * @param  array $attachments
+ * @return bool true if mail has been sent, false if it failed
+ */
+function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
+
+	Mailjet_WP_Mail::send( $to, $subject, $message, $headers, $attachments );
+}
